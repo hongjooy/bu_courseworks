cad4b99b19d28bf000493a1e2d5fc102ef0bd864
Jooyoun Hong <hongjooy@bu.edu>
2016-03-12 16:40:58 -0500

    removed

--- Senior Design/mooove/mooove_eb/website/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Fitbit#, UserProfile
-
-# admin.site.register(Member) 
-admin.site.register(Fitbit)
-#admin.site.register(UserProfile)
